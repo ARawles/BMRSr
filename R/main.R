d3927c360cf98f976fac35ac210e7d9bb333a9c6
Adam Rawles <Adam.Rawles@elexon.co.uk>
2019-06-27 15:42:07 +0100

    Restructured build_call functions. Improved docs

--- R/main.R
+++ R/main.R
@@ -6,15 +6,26 @@
 #'
 #' @param data_item character string; the id of the B flow
 #' @param api_key character string; api key retreived from the Elexon portal
-#' @param ... other values (all character strings) to be passed to the function as API parameters
-#' @return string; created url for the call
+#' @param settlement_date character string; settlement date (automatically cleaned by format_date)
+#' @param settlement_period character string; settlement period
+#' @param year character string; year
+#' @param month character string; month
+#' @param week character string; week
+#' @param process_type character string; process type
+#' @param start_time character string; start time
+#' @param end_time character string; end time
+#' @param start_date character string; start date
+#' @param end_date character string; end date
+#' @param service_type character string; file format (csv or xml)
+#' @param api_version character string; version of the api to use (currently on v1)
+#' @return character string; created url for the call
 #' @examples
 #' build_b_call(data_item = "B1730", api_key = "12345", settlement_date = "14-12-2016")
 
 build_b_call <- function(data_item, api_key, settlement_date = NULL, settlement_period = NULL,
                          year = NULL, month = NULL, week = NULL, process_type = NULL, start_time = NULL,
-                         end_time = NULL, start_date = NULL, end_date = NULL, service_type = "csv", version = "v1") {
-  url = paste0("https://api.bmreports.com/BMRS/", data_item, "/", version, "?APIKey=", api_key)
+                         end_time = NULL, start_date = NULL, end_date = NULL, service_type = "csv", api_version = "v1") {
+  url = paste0("https://api.bmreports.com/BMRS/", data_item, "/", api_version, "?APIKey=", api_key)
   if (!is.null(settlement_date)) {
     url = paste0(url, "&SettlementDate=", format_date(settlement_date))
   }
@@ -53,16 +64,30 @@ build_b_call <- function(data_item, api_key, settlement_date = NULL, settlement_
 #'
 #' @param data_item character string; the id of the REMIT flow
 #' @param api_key character string; api key retreived from the Elexon portal
-#' @param ... other values (all character strings) to be passed to the function as API parameters
+#' @param event_start character string; event start (automatically cleaned by format_date)
+#' @param event_end character string; event end (automatically cleaned by format_date)
+#' @param publication_from character string; publication from (automatically cleaned by format_date)
+#' @param publication_to character string; publication to (automatically cleaned by format_date)
+#' @param participant_id character string; participant id
+#' @param asset_id character string; asset id
+#' @param event_type character string; event type
+#' @param fuel_type character string; fuel type
+#' @param message_type character string; message type
+#' @param message_id character string; message id
+#' @param unavailability_type character string; unavailability type
+#' @param active_flag character string; active flag
+#' @param sequence_id character string; sequence id
+#' @param service_type character string; file format (csv or xml)
+#' @param api_version character string; version of the api to use (currently on v1)
 #' @return string; created url for the call
 #' @examples
 #' build_remit_call(data_item = "MessageListRetrieval", api_key = "12345", settlement_date = "14-12-2016")
 build_remit_call <- function(data_item, api_key, event_start = NULL, event_end = NULL, publication_from = NULL, publication_to = NULL,
                              participant_id = NULL, asset_id =  NULL, event_type = NULL, fuel_type = NULL, message_type = NULL, message_id = NULL,
-                             unavailability_type =  NULL, active_flag = NULL, sequence_id = NULL, service_type = "csv", version = "v1"){
-  url = paste0("https://api.bmreports.com/BMRS/", data_item, "/", version, "?APIKey=", api_key)
-  if (service_id %in% c("MessageListRetrieval", "MessageDetailRetrieval") && service_type == "csv"){
-    warning("This service type is incompatable with this service id. The data will be returned as XML")
+                             unavailability_type =  NULL, active_flag = NULL, sequence_id = NULL, service_type = "xml", api_version = "v1"){
+  url = paste0("https://api.bmreports.com/BMRS/", data_item, "/", api_version, "?APIKey=", api_key)
+  if (service_type == "csv"){
+    stop("Remit files cannot be returned as .csv - service_type must be xml")
   }
   if (!is.null(event_start)) {
     url = paste0(url, "&EventStart=", format_date(event_start))
@@ -113,16 +138,37 @@ build_remit_call <- function(data_item, api_key, event_start = NULL, event_end =
 #'
 #' @param data_item character string; the id of the legacy data
 #' @param api_key character string; api key retreived from the Elexon portal
-#' @param ... other values (all character strings) to be passed to the function as API parameters
+#' @param from_date character string; from date (automatically cleaned by format_date)
+#' @param to_date character string; to date (automatically cleaned by format_date)
+#' @param settlement_date character string; settlement date (automatically cleaned by format_date)
+#' @param settlement_period character string; settlement period
+#' @param bm_unit_id character string; BM Unit ID
+#' @param lead_party_name character string;  lead party name
+#' @param ngc_bm_unit_name character string; NGC BM Unit name
+#' @param from_cleared_date character string; from cleared date (automatically cleaned by format_date)
+#' @param to_cleared_date character string; to cleared dat (automatically cleaned by format_date)
+#' @param is_two_day_window character string; is two day window
+#' @param from_datetime character string; from datetime
+#' @param to_datetime character string; to datetime
+#' @param from_settlement_date character string; from settlement date (automatically cleaned by format_date)
+#' @param to_settlement_date character string; to settlement date (automatically cleaned by format_date)
+#' @param period character string; period
+#' @param fuel_type character string; fuel type
+#' @param balancing_service_volume character string; balancing service volume
+#' @param zone_identifier character string; zone identifier
+#' @param start_time character string; start time
+#' @param trade_type character string; trade type
+#' @param service_type character string; file format (csv or xml)
+#' @param api_version character string; version of the api to use (currently on v1)
 #' @return string; created url for the call
 #' @examples
-#' build_legacy_call(data_item = "FUELINST", api_key = "12345", from_datetime = "14-12-201613:00:00", to_datetime = "14-12-201614:00:00)
+#' build_legacy_call(data_item = "FUELINST", api_key = "12345", from_datetime = "14-12-201613:00:00", to_datetime = "14-12-201614:00:00")
 build_legacy_call <- function(data_item, api_key, from_date = NULL, to_date = NULL, settlement_date = NULL, settlement_period =  NULL, bm_unit_id = NULL, bm_unit_type = NULL,
                               lead_party_name = NULL, ngc_bm_unit_name = NULL, from_cleared_date = NULL, to_cleared_date = NULL,
                               is_two_day_window = NULL, from_datetime = NULL, to_datetime = NULL, from_settlement_date = NULL, to_settlement_date = NULL,
-                              period = NULL, fuel_type = NULL, balancing_service_volume = NULL, zone_identifier = NULL, start_time = NULL,
-                              trade_type = NULL, service_type = "csv"){
-  url = paste0("https://api.bmreports.com/BMRS/", data_item, "/", version, "?APIKey=", api_key)
+                              period = NULL, fuel_type = NULL, balancing_service_volume = NULL, zone_identifier = NULL, start_time = NULL, end_time = NULL,
+                              trade_type = NULL, api_version = "v1", service_type = "csv"){
+  url = paste0("https://api.bmreports.com/BMRS/", data_item, "/", api_version, "?APIKey=", api_key)
   if (!is.null(from_date)) {
     url = paste0(url, "&FromDate=", format_date(from_date))
   }
@@ -183,6 +229,9 @@ build_legacy_call <- function(data_item, api_key, from_date = NULL, to_date = NU
   if (!is.null(start_time)){
     url = paste0(url, "&StartTime=", start_time)
   }
+  if (!is.null(end_time)){
+    url = paste0(url, "&EndTime=", end_time)
+  }
   if (!is.null(trade_type)){
     url = paste0(url, "&TradeType=", trade_type)
   }
@@ -191,19 +240,41 @@ build_legacy_call <- function(data_item, api_key, from_date = NULL, to_date = NU
   }
 }
 
-
-build_call <- function(type, ...){
-  typed_call <- get_function(type)
-  url <- typed_call(list(...))
+#' Build an API call (uses the appropriate function based on the data item)
+#'
+#' @inheritParams build_b_call
+#' @inheritParams  build_remit_call
+#' @inheritParams  build_legacy_call
+#' @example
+#' build_call(data_item = "TEMP", api_key = "12345", from_date = "12 Jun 2018", to_date = "13 Jun 2018", service_type = "csv")
+build_call <- function(...){
+  params <- list(...)
+  typed_call <- get_function(params[['data_item']])
+  url <- do.call(what = typed_call, args = params)
   return(url)
 }
 
-get_function <- function(type){
-  if (type == "B Flow"){
+#' Get the correct function to create the API call depending on the data item
+#' @param data_item character string; data item to be retrieved
+#' @example get_function("TEMP")
+get_function <- function(data_item){
+  if (nchar(data_item) == 5 & substr(data_item,1,1) == "B"){
     return(build_b_call)
   }
-  else if (type == "Remit"){
+  else if (data_item %in% c("MessageDetailRetrieval", "MessageListRetrieval")){
     return(build_remit_call)
+  }
+  else if(data_item %in% c("TEMP", "BOD", "CDN", "SYSWARN", "DISBAD", "NETBSAD", "FREQ", "MID", "DEVINDOD", "NONBM", "QAS", "ROLSYSDEM",
+                           "WINDFORPK", "WINDFORFUELHH","FUELINSTHHCUR", "FUELINST", "FUELHH", "INTERFUELHH", "NOU2T14D", "FOU2T14D",
+                           "UOU2T14D", "NOU2T52W", "FOU2T52W", "UOU2T52W", "NOUY1", "NOUY2", "NOUY3", "NOUY4", "NOUY5", "ZOU2T14D",
+                           "ZOU2T52W", "ZOUY1", "ZOUY2", "ZOUY3", "ZOUY4", "ZOUY5", "INDOITSDO", "MELIMBALNGC", "FORDAYDEM", "DEMMF2T14D",
+                           "DEMMF2T52W", "SOSOP", "SOSOT", "PKDEMYESTTDYTOM", "INDPKDEMINFO", "SYSDEM", "INDTRIADDEMINFO", "PHYBMDATA",
+                           "DYNBMDATA", "DERBMDATA", "DERSYSDATA", "DETSYSPRICES", "MKTDEPTHDATA", "LATESTACCEPTS", "HISTACCEPTS", "SYSMSG",
+                           "BMUNITSEARCH", "SYSWARNTDYTOM", "HISTSYSWARN", "LOLPDRM", "DEMCI", "STORAW", "TRADINGUNIT", "")){
+    return(build_legacy_call)
+  }
+  else{
+    stop("Data item not valid.")
   }
 }
 
@@ -225,7 +296,7 @@ send_request <- function(url) {
 }
 
 
-#' Create B flow API call and retrieve the results
+#' Parse the results of a call (specific for B flows)
 #'
 #' @param response A response() object returned from the API request
 #' @param format The format of the content of the response() object; either "csv" or "xml"
@@ -251,50 +322,21 @@ parse_response <- function(response, format){
 }
 
 
-#' Create B flow API call, send the request and retrieve the results, and parse them
+#' Create an API call, send the request and retrieve the results, and parse them
 #'
-#' @inheritParams build_b_call
+#' @inheritParams build_call
 #' @return A tibble if service_type = "csv", otherwisea list
 #' @examples
-#' get_b(data_item = "B1730", api_key = "12345", settlement_date = "14-12-2016)
+#' full_request(data_item = "B1730", api_key = "12345", settlement_date = "14-12-2016")
 
-get_b <- function(data_item, api_key, settlement_date = NULL, settlement_period = NULL,
-                  year = NULL, month = NULL, week = NULL, process_type = NULL, start_time = NULL,
-                  end_time = NULL, start_date = NULL, end_date = NULL, service_type = "csv", version = "v1"){
-  url <- build_b_call(data_item, api_key, settlement_date, settlement_period,
-                      year, month, week, process_type, start_time,
-                      end_time, start_date, end_date, service_type, version)
+full_request <- function(...){
+  d_item <- list(...)[['data_item']]
+  s_type <- list(...)[['service_type']]
+  url <- do.call(build_call, args = list(...))
   results <- send_request(url)
-  parsed <- parse_response(results, service_type)
+  parsed <- parse_response(results, d_item, s_type)
   return(parsed)
 }
 
-  # structure(
-  #   list(
-  #     request = url,
-  #     response = results$status_code,
-  #     content = parsed
-  #   ),
-  #   class = "BMRS_api"
-  # )
-
-
-
-
-#fields (B Flows):
-    #version
-    #key
-    #settlement date
-    #settlement period
-    #service type
-    #year
-    #month
-    #week
-    #process type
-    #service type
-    #start time
-    #end time
-    #start date
-    #end date
 
 
