b795f40bae1544e48a9337399af517e261091ff8
Adam Rawles <Adam.Rawles@elexon.co.uk>
2019-06-27 14:30:37 +0100

    Added functions for legacy and REMIT flows. Created placeholder functions that will get the correct function for the data item

--- R/main.R
+++ R/main.R
@@ -49,6 +49,171 @@ build_b_call <- function(data_item, api_key, settlement_date = NULL, settlement_
   return(url)
 }
 
+#' Create an API call for REMIT flows
+#'
+#' @param data_item character string; the id of the REMIT flow
+#' @param api_key character string; api key retreived from the Elexon portal
+#' @param ... other values (all character strings) to be passed to the function as API parameters
+#' @return string; created url for the call
+#' @examples
+#' build_remit_call(data_item = "MessageListRetrieval", api_key = "12345", settlement_date = "14-12-2016")
+build_remit_call <- function(data_item, api_key, event_start = NULL, event_end = NULL, publication_from = NULL, publication_to = NULL,
+                             participant_id = NULL, asset_id =  NULL, event_type = NULL, fuel_type = NULL, message_type = NULL, message_id = NULL,
+                             unavailability_type =  NULL, active_flag = NULL, sequence_id = NULL, service_type = "csv", version = "v1"){
+  url = paste0("https://api.bmreports.com/BMRS/", data_item, "/", version, "?APIKey=", api_key)
+  if (service_id %in% c("MessageListRetrieval", "MessageDetailRetrieval") && service_type == "csv"){
+    warning("This service type is incompatable with this service id. The data will be returned as XML")
+  }
+  if (!is.null(event_start)) {
+    url = paste0(url, "&EventStart=", format_date(event_start))
+  }
+  if (!is.null(event_end)) {
+    url = paste0(url, "&EventEnd=", format_date(event_end))
+  }
+  if (!is.null(publication_from)) {
+    url = paste0(url, "&PublicationFrom=", format_date(publication_from))
+  }
+  if (!is.null(publication_to)) {
+    url = paste0(url, "&PublicationTo=", format_date(publication_to))
+  }
+  if (!is.null(participant_id)) {
+    url = paste0(url, "&ParticipantID=", participant_id)
+  }
+  if (!is.null(asset_id)) {
+    url = paste0(url, "&AssetID=", asset_id)
+  }
+  if (!is.null(event_type)) {
+    url = paste0(url, "&EventType=", event_type)
+  }
+  if (!is.null(fuel_type)) {
+    url = paste0(url, "&FuelType=", fuel_type)
+  }
+  if (!is.null(message_type)) {
+    url = paste0(url, "&MessageType=", message_type)
+  }
+  if (!is.null(message_id)) {
+    url = paste0(url, "&MessageID=", message_id)
+  }
+  if (!is.null(unavailability_type)) {
+    url = paste0(url, "&UnavailabilityType=", unavailability_type)
+  }
+  if (!is.null(active_flag)) {
+    url = paste0(url, "&ActiveFlag=", active_flag)
+  }
+  if (!is.null(sequence_id)) {
+    url = paste0(url, "&SequenceId=", sequence_id)
+  }
+  if (!is.null(service_type)){
+    url = paste0(url, "&ServiceType=", service_type)
+  }
+  return(url)
+}
+
+#' Create an API call for legacy data
+#'
+#' @param data_item character string; the id of the legacy data
+#' @param api_key character string; api key retreived from the Elexon portal
+#' @param ... other values (all character strings) to be passed to the function as API parameters
+#' @return string; created url for the call
+#' @examples
+#' build_legacy_call(data_item = "FUELINST", api_key = "12345", from_datetime = "14-12-201613:00:00", to_datetime = "14-12-201614:00:00)
+build_legacy_call <- function(data_item, api_key, from_date = NULL, to_date = NULL, settlement_date = NULL, settlement_period =  NULL, bm_unit_id = NULL, bm_unit_type = NULL,
+                              lead_party_name = NULL, ngc_bm_unit_name = NULL, from_cleared_date = NULL, to_cleared_date = NULL,
+                              is_two_day_window = NULL, from_datetime = NULL, to_datetime = NULL, from_settlement_date = NULL, to_settlement_date = NULL,
+                              period = NULL, fuel_type = NULL, balancing_service_volume = NULL, zone_identifier = NULL, start_time = NULL,
+                              trade_type = NULL, service_type = "csv"){
+  url = paste0("https://api.bmreports.com/BMRS/", data_item, "/", version, "?APIKey=", api_key)
+  if (!is.null(from_date)) {
+    url = paste0(url, "&FromDate=", format_date(from_date))
+  }
+  if (!is.null(to_date)) {
+    url = paste0(url, "&ToDate=", format_date(to_date))
+  }
+  if (!is.null(settlement_date)) {
+    url = paste0(url, "&SettlementDate=", format_date(settlement_date))
+  }
+  if (!is.null(settlement_period)){
+    url = paste0(url, "&SettlementPeriod=", settlement_period)
+  }
+  if (!is.null(bm_unit_id)){
+    url = paste0(url, "&BMUnitID=", bm_unit_id)
+  }
+  if (!is.null(bm_unit_type)){
+    url = paste0(url, "&BMUnitType=", bm_unit_type)
+  }
+  if (!is.null(lead_party_name)){
+    url = paste0(url, "&LeadPartName=", lead_party_name)
+  }
+  if (!is.null(ngc_bm_unit_name)){
+    url = paste0(url, "&NGCBMUnitName=", ngc_bm_unit_name)
+  }
+  if (!is.null(from_cleared_date)){
+    url = paste0(url, "&FromClearedDate=", format_date(from_cleared_date))
+  }
+  if (!is.null(to_cleared_date)){
+    url = paste0(url, "&ToClearedDate=", format_date(to_cleared_date))
+  }
+  if (!is.null(is_two_day_window)){
+    url = paste0(url, "&IsTwoDayWindow=", is_two_day_window)
+  }
+  if (!is.null(from_datetime)){
+    url = paste0(url, "&FromDateTime=", from_datetime)
+  }
+  if (!is.null(to_datetime)){
+    url = paste0(url, "&ToDateTime=", to_datetime)
+  }
+  if (!is.null(from_settlement_date)){
+    url = paste0(url, "&FromSettlementDate=", format_date(from_settlement_date))
+  }
+  if (!is.null(to_settlement_date)){
+    url = paste0(url, "&ToSettlementDate=", format_date(to_settlement_date))
+  }
+  if (!is.null(period)){
+    url = paste0(url, "&Period=", period)
+  }
+  if (!is.null(fuel_type)){
+    url = paste0(url, "&FuelType=", fuel_type)
+  }
+  if (!is.null(balancing_service_volume)){
+    url = paste0(url, "&BalancingServiceVolume=", balancing_service_volume)
+  }
+  if (!is.null(zone_identifier)){
+    url = paste0(url, "&ZoneIdentifier=", zone_identifier)
+  }
+  if (!is.null(start_time)){
+    url = paste0(url, "&StartTime=", start_time)
+  }
+  if (!is.null(trade_type)){
+    url = paste0(url, "&TradeType=", trade_type)
+  }
+  if (!is.null(service_type)){
+    url = paste0(url, "&ServiceType=", service_type)
+  }
+}
+
+
+build_call <- function(type, ...){
+  typed_call <- get_function(type)
+  url <- typed_call(list(...))
+  return(url)
+}
+
+get_function <- function(type){
+  if (type == "B Flow"){
+    return(build_b_call)
+  }
+  else if (type == "Remit"){
+    return(build_remit_call)
+  }
+}
+
+#' Get the required parameters for a data item
+#' @param data_item string of the data item to get the parameters for
+#' @return A list containing the named parameters required for that call
+get_parameters <- function(data_item){
+  return(list(settlement_date = NULL, settlement_period = NULL))
+}
+
 
 #' Send an API request (basically a wrapper to httr:GET)
 #'
@@ -132,28 +297,4 @@ get_b <- function(data_item, api_key, settlement_date = NULL, settlement_period 
     #start date
     #end date
 
-#fields (remit):
-    #version
-    #key
-    #messageId
-    #participantId
-    #SequenceId
-    #ActiveFlag
-    #type
-    #from date
-    #to date
-    #settlement date
-    #settlement period
-    #BM Unit id
-    #BM Unit type
-    #Lead Party Name
-    #NGC BM Unit Name
-    #from cleared date
-    #to cleared date
-    #isTwoDayWindow
-    #from datetime
-    #to datetime
-    #balancing service volume
-    #zone identifier
-    #start time
-    #trade type
+
