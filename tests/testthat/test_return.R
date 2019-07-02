c817b3f56dbf0fb77080e2fb0c14e2cde01c20f5
Adam Rawles <Adam.Rawles@elexon.co.uk>
2019-07-02 16:59:15 +0100

    Removed ALL proxy settings in test_x to allow for Travis-CI testing

--- tests/testthat/test_return.R
+++ tests/testthat/test_return.R
@@ -2,12 +2,12 @@ context("Return accuracy")
 
 
 test_that("Returns list", {
-  expect_true(tibble::is_tibble(full_request(data_item = "B1720", api_key = "test", settlement_date = "12 Jun 2018", period = "1", service_type = "csv", get_params = list(proxy = "proxysg", proxyport = 8080))))
-  expect_true(is.list(full_request(data_item = "B1720", api_key = "test", settlement_date = "12 Jun 2018", period = "1", service_type = "xml", get_params = list(proxy = "proxysg", proxyport = 8080))))
+  expect_true(tibble::is_tibble(full_request(data_item = "B1720", api_key = "test", settlement_date = "12 Jun 2018", period = "1", service_type = "csv")))
+  expect_true(is.list(full_request(data_item = "B1720", api_key = "test", settlement_date = "12 Jun 2018", period = "1", service_type = "xml")))
 })
 
 test_that("Errors/warnings due to incorrect format", {
   expect_error(full_request(data_item = "B1720", api_key = "test", settlement_date = "12 Jun 2018", period = "1", service_type = "test"))
-  expect_warning(full_request(data_item = "REMIT", api_key = "test", settlement_date = "12 Jun 2018", period = "1", service_type = "csv")))
+  expect_warning(full_request(data_item = "REMIT", api_key = "test", settlement_date = "12 Jun 2018", period = "1", service_type = "csv"))
 })
 
