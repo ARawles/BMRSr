50fd8270a8eef3c99180c3abed5616236377fd19
Adam Rawles <Adam.Rawles@elexon.co.uk>
2019-07-12 11:51:00 +0100

    Restructured parse tests (not sure how they were passing in the first place...)

--- tests/testthat/test_return.R
+++ tests/testthat/test_return.R
@@ -4,7 +4,7 @@ context("Return accuracy")
 test_that("Returns list", {
 
   expect_true(is.list(full_request(data_item = "B1720", api_key = "test", settlement_date = "12 Jun 2018", period = "1", service_type = "xml")))
-  expect_true(is.list(full_request(data_item = "MessageListRetrieval", api_key = "test", event_start = "12 Jun 2018", service_type = "csv")))
+  expect_warning(expect_true(is.list(full_request(data_item = "MessageListRetrieval", api_key = "test", event_start = "12 Jun 2018", service_type = "csv"))))
   expect_true(is.list(full_request(data_item = "MessageListRetrieval", api_key = "test", event_start = "12 Jun 2018", service_type = "xml")))
   expect_true(is.list(full_request(data_item = "TEMP", api_key = "test", from_date = "12 Jun 2018", service_type = "xml")))
 })
