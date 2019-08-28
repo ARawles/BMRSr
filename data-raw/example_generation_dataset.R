60a47f4ad0056492b5845e9abc254796b34c70b1
Adam Rawles <Adam.Rawles@elexon.co.uk>
2019-08-28 15:27:57 +0100

    Updated internal data

---
+++ data-raw/example_generation_dataset.R
@@ -0,0 +1,3 @@
+api_key = "kvr6kh6lzhxx3x2"
+
+gen_data <- full_request(data_item = "FUELINST", api_key = api_key, service_type = "csv", from_datetime = "2019-07-01 00:00:00", to_datetime = "2019-07-01 00:00:00")
