5c67f3d3ae196d94b1dc996c39bc1f8b0fe7a8c5
ARawles <adamrawles@hotmail.co.uk>
2019-08-28 15:29:09 +0100

    Delete example_generation_dataset.R

--- data-raw/example_generation_dataset.R
+++
@@ -1,3 +0,0 @@
-api_key = "kvr6kh6lzhxx3x2"
-
-gen_data <- full_request(data_item = "FUELINST", api_key = api_key, service_type = "csv", from_datetime = "2019-07-01 00:00:00", to_datetime = "2019-07-01 00:00:00")
