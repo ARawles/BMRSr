7cf68848c00ff9a1849cdaef2dfa116202fa67a9
Adam Rawles <Adam.Rawles@elexon.co.uk>
2019-08-28 16:10:27 +0100

    Updated documentation

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' An example dataset from BMRS showing generation by fuel type.
+#'
+#' A dataset containing UK generation by fuel type between 1 July 2019 and 3 July 2019 at half-hourly intervals.
+#'
+#' @format A data frame with 8655 rows and 6 variables:
+#' \describe{
+#'   \item{settlement_date}{Settlement Date of the observation}
+#'   \item{settlement_period}{Settlement Period of the observation}
+#'   ...
+#' }
+#' @source \url{ttps://www.bmreports.com/bmrs/?q=help/about-us}
+"generation_dataset_example"
