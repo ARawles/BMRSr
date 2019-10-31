dc746e1d4bfaa32aeb34660514d093be7d040f3f
Adam Rawles <Adam.Rawles@elexon.co.uk>
2019-10-31 14:26:50 +0000

    Fixed the typo in the internal data

--- R/send_receive.R
+++ R/send_receive.R
@@ -21,7 +21,7 @@ send_request <- function(request, config_options = list()) {
   else if (request$data_item %in% c("MessageDetailRetrieval", "MessageListRetrieval")){
     response$data_item_type <- "Remit"
   }
-  else if(request$data_item %in% c("TEMP", "BOD", "CDN", "SYSWARN", "DISBAD", "NETBSAD", "FREQ", "MID", "DEVINDOD", "NONBM", "QAS", "ROLSYSDEM",
+  else if(request$data_item %in% c("TEMP", "BOD", "CDN", "SYSWARN", "DISBSAD", "NETBSAD", "FREQ", "MID", "DEVINDOD", "NONBM", "QAS", "ROLSYSDEM",
                            "WINDFORPK", "WINDFORFUELHH","FUELINSTHHCUR", "FUELINST", "FUELHH", "INTERFUELHH", "NOU2T14D", "FOU2T14D",
                            "UOU2T14D", "NOU2T52W", "FOU2T52W", "UOU2T52W", "NOUY1", "NOUY2", "NOUY3", "NOUY4", "NOUY5", "ZOU2T14D",
                            "ZOU2T52W", "ZOUY1", "ZOUY2", "ZOUY3", "ZOUY4", "ZOUY5", "INDOITSDO", "MELIMBALNGC", "FORDAYDEM", "DEMMF2T14D",
