9c1397b80f3394b8cdc8af29fa62c35e6911db01
ARawles <adamrawles@hotmail.co.uk>
2021-05-20 20:53:30 +0100

    That function will error now

--- tests/testthat/test_parse.R
+++ tests/testthat/test_parse.R
@@ -4,6 +4,6 @@ context("Parsing function accuracy")
 test_that("Error codes", {
   resp <- httr:::response()
   resp$status_code <- 201
- expect_warning(parse_response(resp))
+ expect_error(parse_response(resp))
 
 })
